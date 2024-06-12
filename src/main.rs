8d32729f87e0e635477e56271b6f46f2a98a32bb
TorinM <torinmay@gmail.com>
2024-06-12 14:34:38 -0600

    Added multiprocessing support

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+use tokio::signal;
+use tokio::sync::mpsc;
+
 use pnet::datalink::DataLinkReceiver;
 use pnet::packet::ethernet::EthernetPacket;
 
 mod config;
-mod format;
+mod output;
 mod network;
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::CONFIG.lock().unwrap();
 
-    let interface_name = config.interface.clone();
+    let interface_name = config.interface.clone().expect("Unknown error getting user supplied interface name");
 
     let interface = match network::datalink::get_interface(&interface_name) {
         Ok(i) => i,
@@ -17,9 +24,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         }
     };
 
-    let mut rx: Box<dyn DataLinkReceiver> = network::datalink::open_listener(&interface);
-    loop {
-        match rx.next() {
+    let running = Arc::new(AtomicBool::new(true));
+
+    let mut datalink_rx: Box<dyn DataLinkReceiver> = network::datalink::open_listener(&interface);
+
+    let (tokio_tx, tokio_rx) = mpsc::channel(32);
+    let print_handle = tokio::spawn(output::terminal::print_events(tokio_rx));
+    let tokio_tx_clone = tokio_tx.clone();
+
+    let ctrl_c_handle = tokio::spawn(async move {
+        signal::ctrl_c().await.unwrap();
+        tokio_tx_clone.send(output::terminal::Event::Exit).await.unwrap();
+    });
+
+    while running.load(Ordering::SeqCst) {
+        match datalink_rx.next() {
             Ok(packet) => {
                 let ethernet_frame = match EthernetPacket::new(&packet) {
                     Some(p) => p,
@@ -38,12 +57,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                     }
                 };
 
-                print!("{}[2J", 27 as char);
-                println!("{}", eth);
+                let message = eth.to_string();
+                tokio_tx.send(output::terminal::Event::PrintMessage(message)).await.unwrap();
+
+                // 
+                // println!("{}", eth);
             },
             Err(e) => {
                 println!("An error occurred while reading: {}", e);
             }
         }
     }
+
+    print!("Cleaning up...");
+    ctrl_c_handle.await.unwrap();
+    print_handle.await.unwrap();
+    println!("Done.");
+
+    Ok(())
 }
