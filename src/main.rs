c20d2894f6a2d26576a6d2a9c2a978aae3a98a5b
TorinM <torinmay@gmail.com>
2024-06-11 23:46:18 -0600

    Removed fake_ethernet_frame functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
+
 use pnet::packet::ethernet::EthernetPacket;
+use pnet::datalink::DataLinkReceiver;
 
 mod network;
 
@@ -22,36 +24,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     loop {
         match rx.next() {
             Ok(packet) => {
-                let ethernet_frame;
-                if cfg!(target_os = "macos") // Workaround for macOS to make a fake Ethernet frame
-                    && interface.is_broadcast()
-                    && !interface.is_broadcast()
-                    && ((!interface.is_loopback() && interface.is_point_to_point()) || interface.is_loopback())
-                {
-                    let mut buf: [u8; 1600] = [0u8; 1600];
-                    ethernet_frame = network::datalink::generate_fake_ethernet_frame(&packet, &mut buf, &interface).unwrap().to_immutable();
-                }
-                else {
-                    ethernet_frame = match EthernetPacket::new(&packet) {
-                        Some(p) => p,
-                        None => {
-                            println!("Failed to parse packet.");
-                            continue;
-                        }
-                    };
-                }
-                
-                // let (source_ip, destination_ip) = match network::datalink::get_source_dest_ips_from_ethernet_frame(&eth_packet) {
-                //     Ok((s,d)) => (s, d),
-                //     Err(e) => {
-                //         println!("Failed to get source and destination IPs: {}", e);
-                //         continue;
-                //     }
-                // };
-                // let payload = eth_packet.payload();
+                let ethernet_frame = match EthernetPacket::new(&packet) {
+                    Some(p) => p,
+                    None => {
+                        println!("Failed to parse packet.");
+                        continue;
+                    }
+                };
 
-                // let packet = network::types::IpPacket::new(id, destination_ip, source_ip, payload.to_vec());
-                // println!("{}", packet);
+                let eth = match network::ethernet::handle_ethernet_frame(&interface, &ethernet_frame)
+                {
+                    Ok(e) => e,
+                    Err(e) => {
+                        println!("Failed to handle Ethernet frame: {}", e);
+                        continue;
+                    }
+                };
+
+                println!("{:?}", eth);
+  
             },
             Err(e) => {
                 println!("An error occurred while reading: {}", e);
