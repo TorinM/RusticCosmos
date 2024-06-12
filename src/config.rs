974478536c16783ba9fdf48a2e4aae8a4c79887b
TorinM <torinmay@gmail.com>
2024-06-12 10:22:52 -0600

    Added more input handling

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,15 @@
-use clap::{Arg, Command};
-use once_cell::sync::Lazy;
 use std::sync::Mutex;
+
+use once_cell::sync::Lazy;
+
+use clap::{Arg, Command};
+
+use crate::format::write_file;
 
 #[derive(Debug)]
 pub struct Config {
-    pub interface: String,
-    pub output_file: String
+    pub interface: Option<String>,
+    pub output_file_path: Option<String>
 }
 
 // Define a global, lazily-initialized CONFIG instance
@@ -27,27 +31,22 @@ pub static CONFIG: Lazy<Mutex<Config>> = Lazy::new(|| {
                 .short('o')
                 .long("output")
                 .value_name("OUTPUT FILE")
-                .help("Sets the output file to write to, defaults to output.pcap")
+                .help("Sets the output file to write to. Use relative or absolute path.")
                 .required(false),
         )
         .get_matches();
 
     // Initialize the config based on parsed arguments
-    let interface = match matches.get_one::<String>("interface") {
-        Some(i) => i.clone(),
-        None => {
-            eprintln!("No interface provided.");
-            std::process::exit(1);
-        }
-    };
+    let interface = matches.get_one::<String>("interface").cloned();
 
-    let output_file = match matches.get_one::<String>("output") {
-        Some(o) => o.clone(),
-        None => "output.pcap".to_string()
-    };
+    let output_file_path = matches.get_one::<String>("output").and_then(|path| {
+        write_file::verify_file_path(path).ok()?;
+        write_file::create_file(path).ok()?;
+        Some(path.clone())
+    });
 
     Mutex::new(Config {
         interface,
-        output_file
+        output_file_path
     })
 });
