22bab392212abf616db44b9ad94d9463362efcf8
TorinM <torinmay@gmail.com>
2024-06-12 22:07:12 -0600

    Updated translation

--- src/network/translate.rs
+++ src/network/translate.rs
@@ -1,10 +1,12 @@
-pub fn packet_payload_to_string(payload: &[u8]) -> Result<String, Box<dyn std::error::Error>> {
+use std::string::FromUtf8Error;
+
+pub fn packet_payload_to_string(payload: Vec<u8>) -> Result<String, FromUtf8Error> {
     // let mut payload_string = String::new();
 
-    let payload_string = String::from_utf8_lossy(payload).to_string();
+    String::from_utf8(payload)
 
     // for byte in payload {
     //     payload_string.push_str(&format!("{:02x} ", byte));
     // }
-    Ok(payload_string)
+    // Ok(payload_string)
 }
